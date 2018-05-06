6d88ee56a78b2729f8248d872a789d28baca6709
arthur <arthur.teisseire@epitech.eu>
2018-05-06 18:33:51 +0200

    [*]manage_buttons -- segfault fix + outline disable at first

--- src/loop_menu.c
+++ src/loop_menu.c
@@ -5,11 +5,12 @@
 ** loop_for_menu
 */
 
-#include "rpg.h"
-
 #include <stdio.h>
+#include "rpg.h"
+#include "define.h"
 
-int loop_menu(rpg_t *rpg)
+int loop_menu(rpg_t __attribute((unused))*rpg)
 {
 	printf("cette boucle de printf est indépendante des autres scenes, nous somme dans menu\n");
+	return (SUCCESS);
 }
--- src/scene/manage_buttons.c
+++ src/scene/manage_buttons.c
@@ -5,22 +5,21 @@
 ** manage button
 */
 
+#include <SFML/Graphics.h>
 #include "button.h"
 #include "scene.h"
 #include "define.h"
-#include <SFML/Graphics.h>
 
 int check_over(button_t *button, int x, int y)
 {
 	sfVector2f pos = sfRectangleShape_getPosition(button->rect);
 	sfVector2f rect = sfRectangleShape_getSize(button->rect);
 
-	if (x > pos.x && x < pos.x + rect.x && y > pos.y && y < pos.y + rect.y) {
+	if (x > pos.x && x < pos.x + rect.x && y > pos.y && y < pos.y + rect.y)
 		sfRectangleShape_setOutlineThickness(button->rect, 2.0);
-		return(0);
-	}
-	sfRectangleShape_setOutlineThickness(button->rect, 0.0);
-	return (0);
+	else
+		sfRectangleShape_setOutlineThickness(button->rect, 0.0);
+	return (SUCCESS);
 }
 
 int check_clic(rpg_t *rpg, button_t *button, int x, int y)
@@ -28,26 +27,27 @@ int check_clic(rpg_t *rpg, button_t *button, int x, int y)
 	sfVector2f pos = sfRectangleShape_getPosition(button->rect);
 	sfVector2f rect = sfRectangleShape_getSize(button->rect);
 
-	while (x > pos.x && x < pos.x + rect.x && y > pos.y && y < pos.y + rect.y) {
+	if (x > pos.x && x < pos.x + rect.x && y > pos.y && y < pos.y + rect.y) {
 		sfRectangleShape_setOutlineThickness(button->rect, 4.0);
-		button->button_func(rpg);
-		return (SUCCESS);
-	}
-	return (0);
+		if (button->button_func != NULL)
+			button->button_func(rpg);
+	} else
+		sfRectangleShape_setOutlineThickness(button->rect, 0.0);
+	return (SUCCESS);
 }
 
-int manage_button(rpg_t *rpg, button_t **button, sfEvent *event)
+int manage_button(rpg_t *rpg, button_t **buttons, sfEvent *event)
 {
 	int i = 0;
 
-	while (button[i] != NULL) {
-		if (event->type == sfEvtMouseMoved){
-			check_over(button[i], event->mouseMove.x, event->mouseMove.y);
+	while (buttons[i] != NULL) {
+		if (event->type == sfEvtMouseMoved) {
+			check_over(buttons[i], event->mouseMove.x, event->mouseMove.y);
 		}
-		if (event->type == sfEvtMouseButtonPressed){
-			check_clic(rpg, button[i], event->mouseButton.x, event->mouseButton.y);
+		if (event->type == sfEvtMouseButtonPressed) {
+			check_clic(rpg, buttons[i], event->mouseButton.x, event->mouseButton.y);
 		}
 		i++;
 	}
-	return (0);
+	return (SUCCESS);
 }
