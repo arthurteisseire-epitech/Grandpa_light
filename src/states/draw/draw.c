369362e58054b645017953fc91f752303e85ff71
arthur <arthur.teisseire@epitech.eu>
2018-05-07 11:30:10 +0200

    [*]draw -- player is now draw only if there is a map

--- src/states/draw/draw.c
+++ src/states/draw/draw.c
@@ -9,12 +9,14 @@
 #include "rpg.h"
 #include "define.h"
 #include "states.h"
+#include "scene.h"
 
 int draw(rpg_t *rpg)
 {
 	sfRenderWindow_clear(rpg->window, sfBlack);
 	draw_scene(rpg, rpg->scenes[rpg->curr_scene]);
-	draw_player(rpg);
+	if (rpg->scenes[rpg->curr_scene]->map != NULL)
+		draw_player(rpg);
 	sfRenderWindow_display(rpg->window);
 	return (SUCCESS);
 }
