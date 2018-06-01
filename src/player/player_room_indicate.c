2a4977812d66f6361117e351a26e9fe5e94df8bb
arthur <arthur.teisseire@epitech.eu>
2018-06-01 10:18:03 +0200

    [*]player_room_indicate -- singular and plurial handled

--- src/player/player_room_indicate.c
+++ src/player/player_room_indicate.c
@@ -15,6 +15,16 @@
 #include "tool.h"
 #include "define.h"
 
+static void set_text_help(rpg_t *rpg, player_t *player, sfText *text, char *str)
+{
+	sfText_setString(text, str);
+	sfText_setFont(text, get_font_by_name(rpg->fonts, "default")->font);
+	sfText_setPosition(text,
+	(sfVector2f){player->pos.x * 64, player->pos.y * 64 - 40});
+	sfText_setColor(text, sfWhite);
+	sfText_setCharacterSize(text, 20);
+}
+
 static sfText *text_help(rpg_t *rpg, player_t *player, tile_t *tile)
 {
 	int orb_left = tile->chanel - player->stats->nb_orbe;
@@ -26,15 +36,13 @@ static sfText *text_help(rpg_t *rpg, player_t *player, tile_t *tile)
 	if (orb_left < 0)
 		orb_left = 0;
 	str = my_itoa(orb_left);
-	str = concat(str, " orbs missing to open this door.", 38);
+	if (orb_left == 1)
+		str = concat(str, " orb missing to open this door.", 38);
+	else
+		str = concat(str, " orbs missing to open this door.", 38);
 	if (str == NULL)
 		return (NULL);
-	sfText_setString(text, str);
-	sfText_setFont(text, get_font_by_name(rpg->fonts, "default")->font);
-	sfText_setPosition(text,
-	(sfVector2f){player->pos.x * 64, player->pos.y * 64 - 40});
-	sfText_setColor(text, sfWhite);
-	sfText_setCharacterSize(text, 20);
+	set_text_help(rpg, player, text, str);
 	free(str);
 	return (text);
 }
